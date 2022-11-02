9abcc9fdc1c55b41f77f34d2c4c02ad6b4791c90
PabloMur <polillomurillo@gmail.com>
2022-11-02 20:52:16 -0300

    reste documentacion de los endpoints en postman

--- be-src/lib/algolia.ts
+++ be-src/lib/algolia.ts
@@ -1,12 +1,5 @@
 import algoliasearch from "algoliasearch";
 
-// Connect and authenticate with your Algolia app
 const client = algoliasearch("0URCNQXYHI", "c128a72141bfd708f12945d45d23949f");
 
-// Create a new index and add a record
 export const algoliaIndex = client.initIndex("pets");
-// const record = { objectID: 1, name: "test_record" };
-// index.saveObject(record).wait();
-
-// // Search the index and print the results
-// index.search("test_record").then(({ hits }) => console.log(hits[0]));
