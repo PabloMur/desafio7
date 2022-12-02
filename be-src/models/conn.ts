5a17bef4f8eec0f5106bb004584efcb113e1abf9
PabloMur <polillomurillo@gmail.com>
2022-12-02 06:14:33 -0300

    elephant migration

--- be-src/models/conn.ts
+++ be-src/models/conn.ts
@@ -1,17 +1,14 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize({
-  dialect: "postgres",
-  username: "vccriwfedpzbxt",
-  password: "1d55159fb910abd63ef0c713cc3e7a35cadec19cab2aa5c55d0b05d00a8e6484",
-  database: "dafqqqv0j7p0j",
-  port: 5432,
-  host: "ec2-52-22-136-117.compute-1.amazonaws.com",
-  ssl: true,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-});
+export const sequelize = new Sequelize(
+  "postgres://urwqpdcc:bMcYXRPPMsd6nx4LehZpFtN23JD1iXog@babar.db.elephantsql.com/urwqpdcc"
+);
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+})();
